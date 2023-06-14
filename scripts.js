bc8c1a9c840ed6be77f77c8ee269287f1fac3a60
enRamirez <ricky.ramirez125042@gmail.com>
2023-06-14 11:17:48 -0500

    Adding a winner to TTT

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
-//       ***********************
-//            INSTRUCTIONS
-//       ***********************
-
-// 1. Read the code below one block at a time.
-// 2. Look for the @TODOs, and figure out how to fix them.
-    // next to each @TODO you will find tasks that need to be finished
-
-// The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
 let currentMarker = 'X'
 
 
@@ -28,20 +24,17 @@ const handleClick = (element) => {
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
-  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
+  document.getElementById(id).innerHTML = currentMarker;
+
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+
+  board[row][column] = currentMarker
+
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
-  
-  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
-    document.getElementById(id).innerHTML = currentMarker;
-
-  changeMarker()
+   
+    checkForWin()
 }
 
 
@@ -57,16 +50,6 @@ const changeMarker = () => {
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
 
   const squares = document.getElementsByTagName("td")
   
@@ -82,3 +65,72 @@ const resetBoard = () => {
   }  
   currentMarker ="X"
 }
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+  if (
+  (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+        || 
+  (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O"))
+  {
+  return true;
+} else if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+        || 
+          (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O"))
+  {
+  return true;
+} else if ((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+        || 
+    (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O"))
+  {
+    return true;
+} else {return false;}
+
+}
+
+const verticalWin = () => {
+  if (
+  (board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+        || 
+  (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O"))
+  {
+    return true;
+  } else if ((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+  || 
+(board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O"))
+{
+  return true;
+} else if ((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+|| 
+(board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"))
+{
+  return true;
+} else {return false;}
+}
+
+const diagonalWin = () => {
+  if (
+  (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+        || 
+  (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O"))
+  {
+    return true;
+  } else if ((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || 
+(board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O"))
+{
+  return true;
+} else {return false;}
+}
+
+
+
+
+
